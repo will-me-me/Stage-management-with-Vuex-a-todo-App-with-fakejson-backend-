eaa52d407600131e9506f095696075022843383f
will-me-me <willykaria777@gmail.comgit config --global user.name will-me-megit commitgit config --global user.email willykaria777@gmail.com>
2021-05-28 16:24:44 +0300

    added a background image on the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,37 +1,43 @@
 <template>
-  <div class="about my-6">
-    <h1
-      class="
-        subheading
-        grey--text
-        text--bold text-uppercase text-center text-decoration-underline
-      "
-    >
-      This is an about this Project
-    </h1>
-    <div class="about">
-      <v-expansion-panels focusable inset popout>
-        <v-expansion-panel v-for="(item, i) in 3" :key="i">
-          <v-expansion-panel-header>
-            About The Project</v-expansion-panel-header
-          >
-          <v-expansion-panel-content>
-            <p class="text-capitalize overline">
-              this is a simple Todo app created using Vue.js and Vuitify.Vuex is
-              used in this application for stage management where one is able to
-              add a todo, delete and filter the number of todos in the Ui.
-            </p>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+  <v-app v-bind:style="{ 'background-image': 'url(' + image + ')' }">
+    <div class="about my-6">
+      <h1
+        class="
+          subheading
+          grey--text
+          text--bold text-uppercase text-center text-decoration-underline
+        "
+      >
+        This is an about this Project
+      </h1>
+      <div class="about">
+        <v-expansion-panels focusable inset popout>
+          <v-expansion-panel v-for="(item, i) in 3" :key="i">
+            <v-expansion-panel-header>
+              About The Project</v-expansion-panel-header
+            >
+            <v-expansion-panel-content>
+              <p class="text-capitalize overline">
+                this is a simple Todo app created using Vue.js and Vuitify.Vuex
+                is used in this application for stage management where one is
+                able to add a todo, delete and filter the number of todos in the
+                Ui.
+              </p>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
     </div>
-  </div>
+  </v-app>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 export default {
   name: "todos",
+  data: () => ({
+    image: "will2.jpg",
+  }),
   computed: mapGetters(["alltodos"]),
 };
 </script>
